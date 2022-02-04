8bb58cec9dc9b33d020e7082b1f36452756c68cb
jorddo <jordanbsneed@gmail.com>
2022-02-03 18:35:54 -0600

    adding api fetch js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,25 +2,23 @@ $(document).ready(function () {
   $('#searchBtn').click(function () {
     const searchInput = $('#search-input').val();
     console.log(searchInput);
+
+    let getCityWeather = function () {
+      // format the api url
+      let apiUrl =
+        'api.openweathermap.org/data/2.5/forecast?q=' +
+        searchInput +
+        '&appid=5487746d0675bbfe431f4c709399c088';
+
+      // make request to the url
+      fetch(apiUrl).then(function (response) {
+        response.json().then(function (data) {
+          console.log(data);
+        });
+      });
+    };
+    getCityWeather();
   });
-
-  // let getCityWeather = function (lat, lon) {
-  //   // format the api url
-  //   let apiUrl =
-  //     'pro.openweathermap.org/data/2.5/forecast/hourly?lat=' +
-  //     lat +
-  //     '&lon=' +
-  //     lon +
-  //     '&appid=5487746d0675bbfe431f4c709399c088';
-
-  //   // make request to the url
-  //   fetch(apiUrl).then(function (response) {
-  //     response.json().then(function (data) {
-  //       // console.log(data);
-  //     });
-  //   });
-  // };
-
   // api call FORMAT pro.openweathermap.org/data/2.5/forecast/hourly?lat={lat}&lon={lon}&appid={API key}
 
   // api call EXAMPLE pro.openweathermap.org/data/2.5/forecast/hourly?lat=35&lon=139&appid=5487746d0675bbfe431f4c709399c088
