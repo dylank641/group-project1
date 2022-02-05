c47ccffd812280511dffa99e426cc415d7e49bad
jorddo <jordanbsneed@gmail.com>
2022-02-05 10:38:27 -0600

    switching branches

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,1 @@
-$(document).ready(function () {
-  // letting search city button record text input
-  $('#searchBtn').click(function () {
-    const searchInput = $('#search-input').val();
-    console.log(searchInput);
-  });
-});
+
