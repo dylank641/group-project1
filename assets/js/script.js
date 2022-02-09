b9f19f5956b61d412e74e221b13083b6ffe43be5
Sheroo Bhagia <sheroo@sb-mac.lan>
2022-02-09 09:16:40 -0600

    search history and error correction

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@
 // ******************************
 
 // array to store app data
-var appData = [];
+var priorSearches = [];
 
 // page sections
 var newSearchBtn = document.querySelector("#new-search");
@@ -16,6 +16,13 @@ var restaurantResultsEl = document.querySelector("#restaurant-results");
 var hotelResultsEl = document.querySelector("#hotel-results");
 var cityPhotosEl = document.querySelector('#photos-page');
 var galleryEl = document.querySelector("#gallery");
+var priorSearchesEl = document.querySelector("#search-history");
+var searchStringEl = document.querySelector("#city-input");
+var toDoTitleEl = document.getElementById("to-do-title");
+var weatherTitleEl = document.getElementById("weather-title");
+var restaurantsTitleEl = document.getElementById("restaurants-title");
+var hotelsTitleEl = document.getElementById("hotels-title");
+
 
 
 // weather elements
@@ -28,212 +35,186 @@ var cityUVIndexEl = $("#cityUVIndex");
 var currentDate = moment().format("l");
 
 
-// ******************************
-// Functions
-// ******************************
-
-// load app data
-var loadAppData = function() {
-  appData = localStorage.getItem("CityScoutAppData");
-  if(!appData) {
-    appData = [];
-    var appState = {
-      currentPage: "search-page"
-    }
-    appData.push(appState);
-    return false;
-  }
-  appData = JSON.parse(appData);
-}
-
-// store app data
-var saveAppData = function() {
-  localStorage.setItem("CityScoutAppData", JSON.stringify(appData));
-}
 
 // refresh app
-var refreshApp = function() {
-  switch (appData[0].currentPage) {
-    case "search-page":
-      // show the search page
-      searchPageEl.style.display = "flex";
-      resultsPageEl.style.display = "none";
-      galleryEl.style.display = "none";
-      break;
-    case "results-page":
-      // show the results page
-      searchPageEl.style.display = "none";
-      resultsPageEl.style.display = "flex";
-      galleryEl.style.display = "inline";
-      break;
-  }
+var refreshApp = function () {
+  thingsToDoResultsEl.textContent = "";
+  restaurantResultsEl.textContent = "";
+  hotelResultsEl.textContent = "";
+  cityPhotosEl.textContent = "";
+  searchPageEl.style.display = "none";
+  resultsPageEl.style.display = "flex";
+  galleryEl.style.display = "inline";
 }
 
-// remove old script elements: this is for the google searches
-function removeOldScripts() {
-  $("script").each(function() {
-    if (this.id.includes("googleScript")) {
-      this.parentNode.removeChild(this);
-    }
-  })
+var showResultPg = function () {
+  searchPageEl.style.display = "none";
+  resultsPageEl.style.display = "flex";
+  galleryEl.style.display = "inline";
 }
 
-// handler for the Things to Do google search
-function handleThingsToDo(response) {
-  console.log(response);
-  var thingsToDoEl = document.getElementById("things-to-do-results");
+var showSearchPg = function () {
+  searchPageEl.style.display = "flex";
+  resultsPageEl.style.display = "none";
+  galleryEl.style.display = "none";
+}
+
+
+// handler for the Restaurants google search
+function handleRestaurants(response) {
+  var restaurantsEl = document.getElementById("restaurant-results");
   for (var i = 0; i < 5; i++) {
     var item = response.items[i];
     var index = i + 1;
     var header = "<a href='" + item.image.contextLink + "' target='_blank'>Check it out</a>";
     var imageURL = item.link;
-    
+    var content = "<p>" + item.htmlTitle + "</p>";
+    restaurantsEl.innerHTML += "<div class='card'><a href = '" + item.image.contextLink + "' target='_blank'><div class='card-section'>" + content + "</div></a><img class='bozo' src='" + imageURL + "'>"
+  }
+}
+
+// handler for the Hotels google search
+function handleHotels(response) {
+  var hotelsEl = document.getElementById("hotel-results");
+  for (var i = 0; i < 5; i++) {
+    var item = response.items[i];
+    var index = i + 1;
+    var header = "<a href='" + item.image.contextLink + "' target='_blank'>Check it out</a>";
+    var imageURL = item.link;
+    var content = "<p>" + item.htmlTitle + "</p>";
+    hotelsEl.innerHTML += "<div class='card'><a href = '" + item.image.contextLink + "' target='_blank'><div class='card-section'>" + content + "</div></a><img class='bozo' src='" + imageURL + "'>"
+  }
+}
+
+// handler for the Things to Do google search
+function handleThingsToDo(response) {
+  var thingsToDoEl = document.getElementById("things-to-do-results");
+
+  for (var i = 0; i < 5; i++) {
+    var item = response.items[i];
+    var index = i + 1;
+    var header = "<a href='" + item.image.contextLink + "' target='_blank'>Check it out</a>";
+    var imageURL = item.link;
+
     var content = "<p>" + item.htmlTitle + "</p>";
     thingsToDoEl.innerHTML += "<div class='card'><a href = '" + item.image.contextLink + "' target='_blank'><div class='card-section'>" + content + "</div></a><img class='bozo' src='" + imageURL + "'>"
 
   }
 }
+
+var changeTitle = function () {
+  toDoTitleEl.textContent = "Things to Do in " + searchStringEl.value;
+  weatherTitleEl.textContent = "Weather in " + searchStringEl.value;
+  restaurantsTitleEl.textContent = "Great Restaurants in " + searchStringEl.value;
+  hotelsTitleEl.textContent = "Noteworthy Hotels in " + searchStringEl.value;
+
+
+}
+
 // form submit handler
 function handleCitySearch(event) {
 
   // prevent default form behavior
   event.preventDefault();
-
   // set the view state
-  appData[0].currentPage = "results-page"
-  saveAppData();
-  refreshApp();
+  // appData[0].currentPage = "results-page"
+  var searchString = searchStringEl.value.trim();
 
-  // ********************
-  // Weather
-  // ********************
-  // get search input values
-  var searchString = $('input[name="city-input"]').val();
-  if (!searchString.trim() ) {
+  if (!searchString) {
     // TO DO: replace with with call to modal
-      console.log("Please enter a city!");
+    alert("Please enter a city!");
+    return;
   }
+
   var searchTokens = [];
   if (searchString.includes(",")) {
-      searchTokens = searchString.trim().split(",");
+    searchTokens = searchString.trim().split(",");
   } else {
-      searchTokens[0] = searchString.trim();
-      searchTokens[1] = "";
+    searchTokens[0] = searchString.trim();
+    searchTokens[1] = "";
   }
   var city = searchTokens[0].trim();
   var state = searchTokens[1].trim();
   var country = "";
+  city = city.toLowerCase();
 
   // get coordinates and location name
   var lattitude = 0;
   var longitude = 0;
   var location = "";
-  var apiURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "," + state + "," + country + "&limit=1&appid=41acdc298e9f5bb9684ae3f719dae78a";
+  var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=0f495242c82beba70a7e55f7073bedf1";
   fetch(apiURL)
-  .then(function(response) {
+    .then(function (response) {
       // request was successful
       if (response.ok) {
-          response.json().then(function(data) {
-              try {
-                  lattitude = data[0].lat;
-                  longitude = data[0].lon;
-                  location = data[0].name + ", " + data[0].state;
-                  console.log("Lat: " + lattitude);
-                  console.log("Lon: " + longitude);
-                  console.log("Loc: " + location);
-                  cityNameEl.text(location + " (" + currentDate + ")");
-              } catch(error) {
-                // TO DO: replace with call to modal
-                console.log("Sorry, we could not find any data for that city!");
-              }
-              // get weather data
-              var apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lattitude + "&lon=" + longitude + "&exclude=alerts&units=imperial&lang=en&appid=41acdc298e9f5bb9684ae3f719dae78a";
-              fetch(apiURL)
-              .then(function(response) {
-                  // request was successful
-                  if (response.ok) {
-                      response.json().then(function(data) {
-                          console.log(data);
-                          console.log(data.current.temp);
-                          var icon = data.current.weather[0].icon;
-                          var iconDesc = data.current.weather[0].description;
-                          var temp = "Temp: " + data.current.temp + "&deg;" + "F";
-                          var wind = "Wind: " + data.current.wind_speed + " MPH";
-                          var humidity = "Humidity: " + data.current.humidity + " %";
-                          var uvindex = data.current.uvi;
-                          weatherIcon.html("<img src='https://openweathermap.org/img/wn/" + icon + "@2x.png' width='50px' height='50px' alt='" + iconDesc + "'>");
-                          cityTempEl.html(temp);
-                          cityWindEl.html(wind);
-                          cityHumidityEl.html(humidity);
-                          if (parseFloat(uvindex) <= 2) {
-                              cityUVIndexEl.html("UV Index: <span class='bg-success colorCodedUVIndex'>" + uvindex + "</span>");
-                          } else if (parseFloat(uvindex) <= 8) {
-                              cityUVIndexEl.html("UV Index: <span class='bg-warning colorCodedUVIndex'>" + uvindex + "</span>");
-                          } else {
-                              cityUVIndexEl.html("UV Index: <span class='bg-danger colorCodedUVIndex'>" + uvindex + "</span>");
-                          }
-                          
-                      })
-                  } else {
-                      console.log('Error: Weather Data Not Found');
-                  }
-              })
-              .catch(function(error) {
-                  console.log("Unable to connect to Open Weather One Call API");
-              })
-              // end get weather data
-          })
-      } else {
-          console.log('Error: City Not Found');
-      }
-  })
-  .catch(function(error) {
-      console.log("Unable to connect to Open Weather Geocoding API");
-  });
-  // end get coordinates and location name
-
-  // ********************
-  // Things to Do
-  // ********************
-  var printPhotos = function (data) {
-    for (var i = 0; i < data.length && i < 15; i++) {
-      var photo = document.createElement('img');
-      console.log(data[i].largeImageURL);
-      photo.setAttribute('src', data[i].largeImageURL);
-      photo.setAttribute('width', '600px');
-      photo.setAttribute("class", "test")
-      cityPhotosEl.appendChild(photo);
-    }
-    document.getElementById('new-search').addEventListener("click", function(){
-      photo.remove();
-  
-    })
-  
-  };
-  
-  var getPhotos = function (city) {
-    console.log(city);
-    city = city.replaceAll(' ', '+');
-    //state = state.replaceAll(' ', '+');
-    placeUrl =
-      'https://pixabay.com/api/?key=25546994-2482c2e9e7a32b9d57d2159ef&q=' +
-      city +
-      '&image_type=photo&pretty=true';
-    console.log(placeUrl);
-    fetch(placeUrl).then(function (response) {
-      if (response.ok) {
-        console.log(response);
+        loadSearches();
+        if (!priorSearches.includes(city)) {
+          priorSearches.unshift(city);
+        }
+        changeTitle();
+        saveSearches();
+        refreshApp();
+        showResultPg();
         response.json().then(function (data) {
-          console.log(data);
-          printPhotos(data.hits);
+          if (data.length !== 0) {
+            lattitude = data.coord.lat;
+            longitude = data.coord.lat;
+            $('#cityHigh').html('Low: ' + Math.round(data.main.temp_min));
+            $('#cityLow').html('High: ' + Math.round(data.main.temp_max));
+            $('#cityDescription').html(data.weather[0].description);
+            cityNameEl.text(city + ", " + state + " (" + currentDate + ")");
+            getWeatherData(lattitude, longitude);
+            getPhotos(city);
+            gSearch(city, state);
+          }
         });
-        
-      }
-    });
-  };
-  getPhotos(city);
+      } else {
+        alert("Incorrect City Name, Try again");
+        searchStringEl.value = "";
 
- 
+      }
+    })
+
+    .catch(function (error) {
+      // TO DO: replace with call to modal
+      console.log("error: " + error);
+    });
+
+
+};
+
+// ********************
+// Things to Do
+// ********************
+var printPhotos = function (data) {
+  for (var i = 0; i < data.length && i < 15; i++) {
+    var photo = document.createElement('img');
+    photo.setAttribute('src', data[i].largeImageURL);
+    photo.setAttribute('width', '600px');
+    photo.setAttribute("class", "test")
+    cityPhotosEl.appendChild(photo);
+  }
+
+};
+
+var getPhotos = function (city) {
+  city = city.replaceAll(' ', '+');
+  placeUrl =
+    'https://pixabay.com/api/?key=25546994-2482c2e9e7a32b9d57d2159ef&q=' +
+    city +
+    '&image_type=photo&pretty=true';
+  fetch(placeUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        printPhotos(data.hits);
+      });
+
+    }
+  });
+};
+
+
+var gSearch = function (city, state) {
   // prepare src attribute
   var googleSearch4ThingsToDo = "https://www.googleapis.com/customsearch/v1?key=AIzaSyDFM9Zvcj3uG-iHT16CNPqEEs7z3LhVUYA&cx=84bf6c59ec9dc5496&searchType=image&q=things to do " + city + " " + state + "&callback=handleThingsToDo";
 
@@ -247,36 +228,128 @@ function handleCitySearch(event) {
   // append to body
   document.body.appendChild(googleScriptEl4ThingsToDo);
 
+  // prepare src attribute
+  var googleSearch4Restaurants = "https://www.googleapis.com/customsearch/v1?key=AIzaSyDFM9Zvcj3uG-iHT16CNPqEEs7z3LhVUYA&cx=84bf6c59ec9dc5496&searchType=image&q=restaurants " + city + " " + state + "&callback=handleRestaurants";
+
+  // create the script element
+  var googleScriptEl4Restaurants = document.createElement("script");
+
+  // set the src and id attributes
+  googleScriptEl4Restaurants.src = googleSearch4Restaurants;
+  googleScriptEl4Restaurants.id = "googleSearch4Restaurants";
+
+  // append to body
+  document.body.appendChild(googleScriptEl4Restaurants);
+
+  // ********************
+  // Hotels
+  // ********************
+
+  // prepare src attribute
+  var googleSearch4Hotels = "https://www.googleapis.com/customsearch/v1?key=AIzaSyDFM9Zvcj3uG-iHT16CNPqEEs7z3LhVUYA&cx=84bf6c59ec9dc5496&searchType=image&q=hotels " + city + " " + state + "&callback=handleHotels";
+
+  // create the script element
+  var googleScriptEl4Hotels = document.createElement("script");
+
+  // set the src and id attributes
+  googleScriptEl4Hotels.src = googleSearch4Hotels;
+  googleScriptEl4Hotels.id = "googleSearch4Hotels";
+
+  // append to body
+  document.body.appendChild(googleScriptEl4Hotels);
+
 }
 
 
 
-function handleNewSearch() {
+
+
+var getWeatherData = function (lattitude, longitude) {
+  var apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lattitude + "&lon=" + longitude + "&exclude=alerts&units=imperial&lang=en&appid=41acdc298e9f5bb9684ae3f719dae78a";
+  fetch(apiURL)
+    .then(function (response) {
+      // request was successful
+      if (response.ok) {
+        response.json().then(function (data) {
+          var icon = data.current.weather[0].icon;
+          var iconDesc = data.current.weather[0].description;
+          var temp = "Temp: " + data.current.temp + "&deg;" + "F";
+          var wind = "Wind: " + data.current.wind_speed + " MPH";
+          var humidity = "Humidity: " + data.current.humidity + " %";
+          var uvindex = data.current.uvi;
+          weatherIcon.html("<img src='https://openweathermap.org/img/wn/" + icon + "@2x.png' width='50px' height='50px' alt='" + iconDesc + "'>");
+          cityTempEl.html(temp);
+          cityWindEl.html(wind);
+          cityHumidityEl.html(humidity);
+          if (parseFloat(uvindex) <= 2) {
+            cityUVIndexEl.html("UV Index: <span class='bg-success colorCodedUVIndex'>" + uvindex + "</span>");
+          } else if (parseFloat(uvindex) <= 8) {
+            cityUVIndexEl.html("UV Index: <span class='bg-warning colorCodedUVIndex'>" + uvindex + "</span>");
+          } else {
+            cityUVIndexEl.html("UV Index: <span class='bg-danger colorCodedUVIndex'>" + uvindex + "</span>");
+          }
+
+        })
+      } else {
+        console.log('Error: Weather Data Not Found');
+      }
+    })
+    .catch(function (error) {
+      console.log("Unable to connect to Open Weather One Call API");
+    })
+
+}
+var loadSearches = function () {
+  priorSearches = JSON.parse(localStorage.getItem("priorSearches") || "[]");
+  if (priorSearches == null) { priorSearches = [] };
+  // Limits search history to 8 hits
+  if (priorSearches.length > 5) {
+    priorSearches.pop();
+  }
+};
+
+var saveSearches = function () {
+  localStorage.setItem("priorSearches", JSON.stringify(priorSearches));
+}
+
+var displayPriorSearches = function () {
+  loadSearches();
+  priorSearchesEl.textContent = "";
+  for (var i = 0; i < priorSearches.length; i++) {
+    var searchBtn = document.createElement("button");
+    searchBtn.className += "prior-search-btn upper-case";
+    searchBtn.setAttribute("type", "button");
+    searchBtn.textContent = priorSearches[i];
+    priorSearchesEl.appendChild(searchBtn);
+  };
+}
+
+function handleNewSearch(event) {
   // set the view state
-  appData[0].currentPage = "search-page"
-  saveAppData();
-  refreshApp();
-  document.getElementById("photos-page").innerHTML = "";
+  searchStringEl.value = "";
+  // event.preventDefault();
+  // showSearchPg();
+  location.reload();
+};
+
+var searchBtnHandler = function (event) {
+  searchStringEl.value = event.target.textContent;
+  handleCitySearch(event);
 }
-
-//clears things to do section when new search is clicked
-document.getElementById("new-search").addEventListener("click", function(){
-  document.getElementById("things-to-do-results").innerHTML = "";
-})
-
-
-
 
 // ******************************
 // Action Listeners
 // ******************************
 citySearchEl.addEventListener("submit", handleCitySearch);
 newSearchBtn.addEventListener("click", handleNewSearch);
+priorSearchesEl.addEventListener("click", searchBtnHandler);
 
 
+showSearchPg();
+displayPriorSearches();
 
 // ******************************
 // Function Calls
 // ******************************
-loadAppData();
-refreshApp();
+showSearchPg();
+displayPriorSearches();
